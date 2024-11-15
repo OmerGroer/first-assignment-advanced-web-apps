eb6c6e77bf232e369353a697348ba3109bbc9f88
Omer Groer <drutr123@gmail.com>
2024-11-15 12:50:12 +0200

    get post by sender

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -1,8 +1,13 @@
 const PostModel = require("../models/postsModel");
 
 const getAllPosts = async (req, res) => {
+  const sender = req.query.sender;
+  
   try {
-    const posts = await PostModel.find();
+    const filter = {}
+    if (sender) filter.sender = sender
+
+    const posts = await PostModel.find(filter);
     res.send(posts);
   } catch (error) {
     res.status(400).send(error.message);
