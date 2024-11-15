67ad9d09397bdcc75c519f2817f6d65dd9f2fff2
Omer Groer <drutr123@gmail.com>
2024-11-15 12:04:34 +0200

    schema and stabs function for posts

---
+++ controllers/postsController.js
@@ -0,0 +1,28 @@
+const PostModel = require("../models/posts_model");
+
+const getAllPosts = async (req, res) => {
+  console.log("get all posts");
+  res.send("get all posts");
+};
+
+const getPostById = async (req, res) => {
+  cconsole.log("get post by id");
+  res.send("get post by id");
+};
+
+const createPost = async (req, res) => {
+  console.log("create a post");
+  res.send("create a post");
+};
+
+const updatePost = (req, res) => {
+  console.log("delete a post");
+  res.send("delete a post");
+};
+
+module.exports = {
+  getAllPosts,
+  createPost,
+  updatePost,
+  getPostById,
+};
