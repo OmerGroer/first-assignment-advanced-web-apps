c30ad8953fd0d21ffffe5bac08d179ad744c8393
galnaor86 <galnaor86@gmail.com>
2025-01-31 13:43:43 +0200

    production

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,18 @@
 import initApp from "./server";
+import https from "https";
+import fs from "fs";
 const port = process.env.PORT;
 
 initApp().then((app) => {
-  app.listen(port, () => {
-    console.log(`App listening at http://localhost:${port}`);
-  });
+  if (process.env.NODE_ENV != "production") {
+    app.listen(port, () => {
+      console.log(`App listening at http://localhost:${port}`);
+    });
+  } else {
+    const prop = {
+      key: fs.readFileSync("../client-key.pem"),
+      cert: fs.readFileSync("../client-cert.pem")
+    }
+    https.createServer(prop, app).listen(port)
+  }
 });
